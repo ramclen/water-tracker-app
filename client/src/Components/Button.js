7d8f4bb2d785b1c1e5df958c33b12c3cc254f492
Cristopher <cristopher.correo@gmail.com>
2020-06-14 21:50:16 +0100

    Increment button font size

--- client/src/Components/Button.js
+++ client/src/Components/Button.js
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import './button.css'
 const Button = ({ onClick, symbol, className }) => {
   return (
     <div className={`col-6 ${className}`}>
-      <button onClick={onClick} className="button">{symbol}</button>
+      <button onClick={onClick} className="button">
+        <span>{symbol}</span>
+      </button>
     </div>
   )
 }
