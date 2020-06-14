7d87baad66fbf45353c68294d17f2749654d5a8a
Cristopher <cristopher.correo@gmail.com>
2020-06-14 12:33:03 +0100

    added update

---
+++ aws/water-levels/update.js
@@ -0,0 +1,67 @@
+"use strict";
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+/**
+ * It update the water level value on DynamoDB and return the new object.
+ */
+module.exports.update = (event, context, callback) => {
+  const data = JSON.parse(event.body);
+
+  // validating the received object
+  if (typeof data.level !== 'string') {
+    console.error('Validation Failed');
+    callback(null, {
+      statusCode: 400,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Couldn\'t update the item.',
+    });
+    return;
+  }
+
+  const params = createParams(event.pathParameters.id, data.level);
+
+  dynamoDb.update(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t fetch the item.',
+      });
+      return;
+    }
+
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(result.Attributes),
+    });
+  });
+
+};
+
+/**
+ * It returns an params object to update the item in DynamoDB
+ * @param  {string} id
+ * @param  {string} level
+ * @returns {Object} 
+ */
+function createParams(id, level) {
+  const timestamp = new Date().getTime();
+  return {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
+      id,
+    },
+    ExpressionAttributeNames: {
+      '#water_level': 'level',
+    },
+    ExpressionAttributeValues: {
+      ':level': level,
+      ':updatedAt': timestamp,
+    },
+    UpdateExpression: 'SET #water_level = :level, updatedAt = :updatedAt',
+    ReturnValues: 'ALL_NEW',
+  };
+}
