4196a69eb12505ffefcd6e28ab1c2ccf24565893
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-27 17:39:08 +0000

    Deploy ramclen/water-tracker-app to github.com/ramclen/water-tracker-app.git:gh-pages

---
+++ precache-manifest.8c49eaa1b5e8fd8d6bcffc8f21e0921f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25772f7589aa437a10819fd80a60d8f0",
+    "url": "/water-tracker-app/index.html"
+  },
+  {
+    "revision": "1cd99a815710dbebf74c",
+    "url": "/water-tracker-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "23ee1c5d088bf535c7e0",
+    "url": "/water-tracker-app/static/css/main.a8b5cab2.chunk.css"
+  },
+  {
+    "revision": "1cd99a815710dbebf74c",
+    "url": "/water-tracker-app/static/js/2.c7e508cd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/water-tracker-app/static/js/2.c7e508cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23ee1c5d088bf535c7e0",
+    "url": "/water-tracker-app/static/js/main.10012cf1.chunk.js"
+  },
+  {
+    "revision": "261fe7d97710bb8def80",
+    "url": "/water-tracker-app/static/js/runtime-main.b339009d.js"
+  }
+]);
